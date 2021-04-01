cbe717347d6d9dba631f0d3bd583a3f215606b5a
Andre Oliveira <andreoliv.machado@gmail.com>
2021-04-01 18:38:39 -0300

    Exclude null values from json response.

--- src/main/java/com/avenuecode/personal/baseservice/model/User.java
+++ src/main/java/com/avenuecode/personal/baseservice/model/User.java
@@ -1,5 +1,6 @@
 package com.avenuecode.personal.baseservice.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
 
     private Long id;
